275754e55cad59b135fe48517c5b7963e7e68530
dan <i@shanhh.com>
2017-04-10 14:36:22 +0800

    feature add application

---
+++ src/main/java/com/shanhh/demo/Application.java
@@ -0,0 +1,15 @@
+package com.shanhh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dan
+ * @since 2017-04-10 14:35
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
